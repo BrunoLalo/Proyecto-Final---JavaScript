7be102ab3be30dadd6f0fb14b87fd97064994a3d
Bruno Lalomia <lalomiabruno@gmail.com>
2023-07-25 12:34:09 -0300

    agregue darkmode

--- main.js
+++ main.js
@@ -4,26 +4,6 @@ const nombre = document.querySelector('#nombre');
 const pesoInput = document.getElementById('peso');
 const alturaInput = document.getElementById('altura');
 
-function mostrarResultado(nombre, resultado, condicion){
-    let datosPantalla = localStorage.getItem('resultadoIMC');
-    const datosGuardados = document.createElement("ul");
-    datosGuardados.classList.add('datos');
-
-    main.innerHTML += "<li class='datos'>" + nombre.value + " " + resultado + " " + condicion + "</li>";
-}
-
-
-
-function guardarResultado(resultado, condicion) {
-    const datos = {
-        nombre: nombre.value,
-        resultado: resultado.toFixed(2),
-        condicion: condicion
-    };
-    const datosJSON = JSON.stringify(datos);
-    localStorage.setItem('resultadoIMC', datosJSON);
-}
-
 calculo.addEventListener("click", () => {
     const peso = parseInt(pesoInput.value);
     const altura = parseInt(alturaInput.value) / 100;
@@ -69,8 +49,37 @@ calculo.addEventListener("click", () => {
 
     main.innerHTML += "<p class='mensaje'>" + "Tu IMC es de: " + res.toFixed(2) + ", y tu condición " + resultado + "</p>";
 
-    guardarResultado(res, resultado);
-    mostrarResultado(nombre, res, resultado)
-
 });
 
+const botonColorMode = document.querySelector("#color");
+const body = document.body;
+
+let darkMode = localStorage.getItem("dark-mode");
+
+function activarDarkMode() {
+    body.classList.add("dark-mode");
+    localStorage.setItem("dark-mode", "activado");
+    botonColorMode.innerText = "Cambiar a Light Mode"
+}
+
+function desactivarDarkMode() {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("dark-mode", "desactivado");
+    botonColorMode.innerText = "Cambiar a Dark Mode"
+}
+
+if (darkMode === "activado") {
+    activarDarkMode();
+} else {
+    desactivarDarkMode();
+}
+
+botonColorMode.addEventListener("click", () => {
+    darkMode = localStorage.getItem("dark-mode");
+
+    if (darkMode === "activado") {
+        desactivarDarkMode();
+    } else {
+        activarDarkMode();
+    }
+})
