12a95ff0b1a41e997c4b65370d0a438d12580729
Bruno Lalomia <lalomiabruno@gmail.com>
2023-08-09 12:18:17 -0300

    Mas local storage

--- main.js
+++ main.js
@@ -4,6 +4,30 @@ const nombre = document.querySelector('#nombre');
 const pesoInput = document.getElementById('peso');
 const alturaInput = document.getElementById('altura');
 
+const resultadosIMC = []
+function guardarResultado(resultado, condicion) {
+    const datos = {
+        nombre: nombre.value,
+        resultado: resultado.toFixed(2),
+        condicion: condicion
+    };
+    resultadosIMC.push(datos)
+    const datosJSON = JSON.stringify(resultadosIMC);
+    localStorage.setItem('resultadoIMC', datosJSON);
+}
+function mostrarResultado() {
+
+    let datosPantalla = localStorage.getItem('resultadoIMC');
+    resultadosIMC.forEach(resultado => {
+        const datosGuardados = document.createElement("ul");
+        datosGuardados.classList.add('datos');
+
+        main.innerHTML += "<h5 class='mensaje'>" + "Tu anterior cálculo fue:" + "</h5>";
+        main.innerHTML += "<li class='datos'>" + resultado.nombre + " " + resultado.resultado + " " + resultado.condicion + "</li>";
+    })
+}
+
+
 calculo.addEventListener("click", () => {
     const peso = parseInt(pesoInput.value);
     const altura = parseInt(alturaInput.value) / 100;
@@ -17,7 +41,7 @@ calculo.addEventListener("click", () => {
                 resultado = "peso inferior al normal.";
             }
             else if (res >= 20 && res < 24) {
-                resultado = "peso Normal.";
+                resultado = "peso normal.";
             }
             else if (res >= 24 && res < 29) {
                 resultado = "peso superior al normal.";
@@ -49,6 +73,8 @@ calculo.addEventListener("click", () => {
 
     main.innerHTML += "<p class='mensaje'>" + "Tu IMC es de: " + res.toFixed(2) + ", y tu condición " + resultado + "</p>";
 
+    guardarResultado();
+    mostrarResultado();
 });
 
 const botonColorMode = document.querySelector("#color");
@@ -83,3 +109,4 @@ botonColorMode.addEventListener("click", () => {
         activarDarkMode();
     }
 })
+
